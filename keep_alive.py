a294c2bf985660ba2b612d24b655fe87af459b2b
yathinkrishnak8-glitch <yathinkrishnak8@gmail.com>
2026-05-02 00:47:33 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,3 +1,18 @@
-discord.py
-groq
-Flask
+from flask import Flask
+from threading import Thread
+import os
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "habbibi mod (: is online and ready to troll on Render."
+
+def run():
+    # Render assigns a random port, this grabs it dynamically!
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
