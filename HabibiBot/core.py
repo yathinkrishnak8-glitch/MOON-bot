0a5c2e913746d26ca62af9f91dc28563eb2973a9
yathinkrishnak8-glitch <yathinkrishnak8@gmail.com>
2026-05-03 23:24:44 +0530

    Delete core.py

--- HabibiBot/core.py
+++
@@ -1,9 +0,0 @@
-# HabibiBot core - database, GIFs, UI Buttons (placeholder)
-
-# This file will hold database connections, GIF lists and UI button definitions.
-
-DB_PLACEHOLDER = True
-GIFS = []
-
-if __name__ == "__main__":
-    print("HabibiBot core - placeholder")
